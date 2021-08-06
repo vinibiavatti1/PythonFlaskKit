2eee4c71f9cd89feeb4c8ea7247ace3c990a6d26
Vinícius Biavatti <vinicius.biavatti@create.pt>
2021-08-06 13:41:00 +0100

    blueprints registry

--- app.py
+++ app.py
@@ -1,44 +1,9 @@
 from flask import Flask
 from project.config.config import config
-from project.processors import blueprint as processors
-from project.handlers import blueprint as handlers
-from project.controllers.public import (
-    auth_ctrl,
-    homepage_ctrl,
-    locale_ctrl,
-)
-from project.controllers.admin import (
-    map_ctrl,
-    chart_ctrl,
-    list_ctrl,
-    form_ctrl,
-)
+from project.blueprints import register_blueprints
 
 
-###############################################################################
-# Init flask
-###############################################################################
-
-
-# Init flask and set the secret key
+# Init Flask application
 app = Flask(__name__)
 app.secret_key = config['secret_key']
-
-
-###############################################################################
-# Register blueprints
-###############################################################################
-
-
-# Register configuration
-app.register_blueprint(processors)
-app.register_blueprint(handlers)
-
-# Register controllers
-app.register_blueprint(homepage_ctrl.blueprint)
-app.register_blueprint(auth_ctrl.blueprint)
-app.register_blueprint(locale_ctrl.blueprint, url_prefix='/locale')
-app.register_blueprint(map_ctrl.blueprint, url_prefix='/admin/map')
-app.register_blueprint(chart_ctrl.blueprint, url_prefix='/admin/chart')
-app.register_blueprint(list_ctrl.blueprint, url_prefix='/admin/list')
-app.register_blueprint(form_ctrl.blueprint, url_prefix='/admin/form')
+register_blueprints(app)
---
+++ project/blueprints.py
@@ -0,0 +1,47 @@
+from flask import Flask
+from project.processors import blueprint as processors
+from project.handlers import blueprint as handlers
+from project.controllers.public import (
+    auth_ctrl,
+    homepage_ctrl,
+    locale_ctrl,
+)
+from project.controllers.admin import (
+    map_ctrl,
+    chart_ctrl,
+    list_ctrl,
+    form_ctrl,
+)
+
+
+def register_blueprints(app: Flask) -> None:
+    """
+    Register blueprints of project into flask application.
+    """
+    app.register_blueprint(
+        processors
+    )
+    app.register_blueprint(
+        handlers
+    )
+    app.register_blueprint(
+        homepage_ctrl.blueprint
+    )
+    app.register_blueprint(
+        auth_ctrl.blueprint
+    )
+    app.register_blueprint(
+        locale_ctrl.blueprint, url_prefix='/locale'
+    )
+    app.register_blueprint(
+        map_ctrl.blueprint, url_prefix='/admin/map'
+    )
+    app.register_blueprint(
+        chart_ctrl.blueprint, url_prefix='/admin/chart'
+    )
+    app.register_blueprint(
+        list_ctrl.blueprint, url_prefix='/admin/list'
+    )
+    app.register_blueprint(
+        form_ctrl.blueprint, url_prefix='/admin/form'
+    )
